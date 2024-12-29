67de4813ccda21f89eac0cdd0557ae3292408e8b
Symlinktech LLP <symlinktech@gmail.com>
2024-12-29 10:14:24 +0530

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Ayanesh Sarkar on 29/12/24.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
